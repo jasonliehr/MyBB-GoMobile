7701f977e82bde642dc8842689ca0d62e7bbf428
Scoutie44 <scoutie44@hotmail.com>
2011-03-10 16:24:53 -0500

    Mainly changed 'regex' to 'string'

--- Upload/inc/languages/english/gomobile.lang.php
+++ Upload/inc/languages/english/gomobile.lang.php
@@ -33,18 +33,17 @@
 
 // ACP Strings
 $l['gomobile'] = "MyBB GoMobile";
-$l['gomobile_sidemenu'] = "GoMobile Regex List";
-$l['gomobile_desc'] = "MyBB GoMobile's accompanying plugin, forces user agents matching a regex to use a certain theme.";
-$l['gomobile_regex'] = "Regular Expression";
-$l['gomobile_addnew'] = "Add New Regex";
-$l['gomobile_edit'] = "Edit Regex";
-$l['gomobile_save'] = "Save Regex";
-$l['gomobile_delete'] = "Delete Regex";
-$l['gomobile_noexist'] = "That regex doesn't exist!";
-$l['gomobile_regex_desc'] = "Enter the regex that GoMobile should send to the mobile theme.";
-$l['gomobile_noregex'] = "Please enter a regex.";
-$l['gomobile_saved'] = "Regex successfully added!";
-$l['gomobile_mobile_version'] = "Mobile Version";
+$l['gomobile_sidemenu'] = "GoMobile Tools";
+$l['gomobile_desc'] = "The free mobile MyBB solution.";
+$l['gomobile_string'] = "User Agent String";
+$l['gomobile_addnew'] = "Add New String";
+$l['gomobile_edit'] = "Edit String";
+$l['gomobile_save'] = "Save String";
+$l['gomobile_delete'] = "Delete String";
+$l['gomobile_noexist'] = "That string doesn't exist!";
+$l['gomobile_string_desc'] = "Enter the User Agent String that GoMobile should send to the mobile theme.";
+$l['gomobile_nostring'] = "Please enter a string.";
+$l['gomobile_saved'] = "String successfully added!";
 
 // Setting Strings
 $l['gomobile_settings_mobile_name_title'] = "Mobile Board Name";
@@ -55,12 +54,15 @@ $l['gomobile_settings_redirect_location_title'] = "Redirect Location";
 $l['gomobile_settings_redirect_location'] = "Enter the location (relative) that you would like to redirect users to. Ignore this if the redirect option above is not enabled.";
 $l['gomobile_settings_theme_id_title'] = "Theme ID";
 $l['gomobile_settings_theme_id'] = "Enter the tid (Theme ID) of GoMobile below. This is used to switch the user to the mobile version.";
+$l['gomobile_settings_permstoggle_title'] = "Use theme permissions?";
+$l['gomobile_settings_permstoggle'] = "Enable this setting to use the theme-specific usage permissions. If disabled, all groups will be able to use GoMobile. Selecting NO also saves a query, and is recommended unless you require theme permissions.";
 $l['gomobile_settings_homename_title'] = "Home Name";
 $l['gomobile_settings_homename'] = "This is the text that will appear in the footer of GoMobile in place of the home link. It is recommended that you keep this to as few charaters as possible.";
 $l['gomobile_settings_homelink_title'] = "Home Link";
 $l['gomobile_settings_homelink'] = "Below is the link that appears in the footer of GoMobile.";
 
 // Misc. strings used through GoMobile
+$l['gomobile_mobile_version'] = "Mobile Version";
 $l['gomobile_ucp'] = "UCP";
 $l['gomobile_mcp'] = "MCP";
 $l['gomobile_acp'] = "ACP";
